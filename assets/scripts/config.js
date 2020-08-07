565bfdcde7e12328fb39bb5af248368afa4d5769
perryfhuang <perryfhuang@gmail.com>
2020-08-07 13:11:25 -0400

    Added picture carousel
    Also added production API url to config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://whispering-brook-82557.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
