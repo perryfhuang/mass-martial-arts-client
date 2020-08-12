7b47bd592e7c79ec2526157321fd34985ee0801e
perryfhuang <perryfhuang@gmail.com>
2020-08-12 15:33:47 -0400

    Disabled clicking out of successful update/delete gym modal
    disabled esc-ing out too. This makes user manually refresh the gym index
    
    Find a better way to dynamically/automatically update gym index in the future.

--- assets/scripts/gym/ui.js
+++ assets/scripts/gym/ui.js
@@ -14,7 +14,6 @@ const getGymsModalHandlebarsTemplate = require('../handlebars/get-gyms-modal.han
 const api = require('./api')
 
 const createGymSuccess = function (response) {
-  $('.message').text('Your gym was added to the database!')
   $('form').trigger('reset')
   const gymHtml = gymHandlebarsTemplate({ gym: response.gym })
   // APPEND NEW GYM TO CURRENT LIST
@@ -23,7 +22,6 @@ const createGymSuccess = function (response) {
   $('#createGymSuccess').modal('show')
 }
 const createGymFail = function () {
-  $('.message').text('Failed to create gym.')
   $('#createGymModal').modal('hide')
   $('#createGymFail').modal('show')
   $('form').trigger('reset')
@@ -35,132 +33,110 @@ const showGymsSuccess = function (response) {
   $('#getGymsSuccess').modal('show')
   const gymsHtml = gymsHandlebarsTemplate({ gyms: response.gyms })
   $('.gyms-list').html(gymsHtml)
-  $('.message').text('Retrieved all gyms from database')
   $('form').trigger('reset')
 }
-const showGymsFail = function (error) {
-  $('.message').text('Failed to retrieve gyms from database.\nSee error message: ' + error.message)
+const showGymsFail = function () {
   $('form').trigger('reset')
 }
 
 const showMMAGymsSuccess = function (response) {
   const gymsHtml = gymsMMAHandlebarsTemplate({ gyms: response.gyms })
   $('.gyms-list').html(gymsHtml)
-  $('.message').text('Retrieved all MMA gyms from database')
   $('form').trigger('reset')
   $('#getMMAGyms').modal('show')
 }
-const showMMAGymsFail = function (error) {
-  $('.message').text('Failed to retrieve gyms from database.\nSee error message: ' + error.message)
+const showMMAGymsFail = function () {
   $('form').trigger('reset')
 }
 
 const showBoxingGymsSuccess = function (response) {
   const gymsHtml = gymsBoxingHandlebarsTemplate({ gyms: response.gyms })
   $('.gyms-list').html(gymsHtml)
-  $('.message').text('Retrieved all boxing gyms from database')
   $('form').trigger('reset')
   $('#getBoxingGyms').modal('show')
 }
-const showBoxingGymsFail = function (error) {
-  $('.message').text('Failed to retrieve gyms from database.\nSee error message: ' + error.message)
+const showBoxingGymsFail = function () {
   $('form').trigger('reset')
 }
 
 const showBJJGymsSuccess = function (response) {
   const gymsHtml = gymsBJJHandlebarsTemplate({ gyms: response.gyms })
   $('.gyms-list').html(gymsHtml)
-  $('.message').text('Retrieved all BJJ gyms from database')
   $('form').trigger('reset')
   $('#getBJJSuccess').modal('show')
 }
-const showBJJGymsFail = function (error) {
-  $('.message').text('Failed to retrieve gyms from database.\nSee error message: ' + error.message)
+const showBJJGymsFail = function () {
   $('form').trigger('reset')
 }
 
 const showJudoGymsSuccess = function (response) {
   const gymsHtml = gymsJudoHandlebarsTemplate({ gyms: response.gyms })
   $('.gyms-list').html(gymsHtml)
-  $('.message').text('Retrieved all judo gyms from database')
   $('form').trigger('reset')
   $('#getJudoGyms').modal('show')
 }
-const showJudoGymsFail = function (error) {
-  $('.message').text('Failed to retrieve gyms from database.\nSee error message: ' + error.message)
+const showJudoGymsFail = function () {
   $('form').trigger('reset')
 }
 
 const showKarateGymsSuccess = function (response) {
   const gymsHtml = gymsKarateHandlebarsTemplate({ gyms: response.gyms })
   $('.gyms-list').html(gymsHtml)
-  $('.message').text('Retrieved all karate gyms from database')
   $('form').trigger('reset')
   $('#getKarateGyms').modal('show')
 }
-const showKarateGymsFail = function (error) {
-  $('.message').text('Failed to retrieve gyms from database.\nSee error message: ' + error.message)
+const showKarateGymsFail = function () {
   $('form').trigger('reset')
 }
 
 const showKickboxingGymsSuccess = function (response) {
   const gymsHtml = gymsKickboxingHandlebarsTemplate({ gyms: response.gyms })
   $('.gyms-list').html(gymsHtml)
-  $('.message').text('Retrieved all kickboxing gyms from database')
   $('form').trigger('reset')
   $('#getKickboxingGyms').modal('show')
 }
-const showKickboxingGymsFail = function (error) {
-  $('.message').text('Failed to retrieve gyms from database.\nSee error message: ' + error.message)
+const showKickboxingGymsFail = function () {
   $('form').trigger('reset')
 }
 
 const showMuayThaiGymsSuccess = function (response) {
   const gymsHtml = gymsMuayThaiHandlebarsTemplate({ gyms: response.gyms })
   $('.gyms-list').html(gymsHtml)
-  $('.message').text('Retrieved all Muay Thai gyms from database')
   $('form').trigger('reset')
   $('#getMuayThaiGyms').modal('show')
 }
-const showMuayThaiGymsFail = function (error) {
-  $('.message').text('Failed to retrieve gyms from database.\nSee error message: ' + error.message)
+const showMuayThaiGymsFail = function () {
   $('form').trigger('reset')
 }
 
 const showTKDGymsSuccess = function (response) {
   const gymsHtml = gymsTKDHandlebarsTemplate({ gyms: response.gyms })
   $('.gyms-list').html(gymsHtml)
-  $('.message').text('Retrieved all Tae Kwon Do gyms from database')
   $('form').trigger('reset')
   $('#getTKDGyms').modal('show')
 }
-const showTKDGymsFail = function (error) {
-  $('.message').text('Failed to retrieve gyms from database.\nSee error message: ' + error.message)
+const showTKDGymsFail = function () {
   $('form').trigger('reset')
 }
 
 const showWrestlingGymsSuccess = function (response) {
   const gymsHtml = gymsWrestlingHandlebarsTemplate({ gyms: response.gyms })
   $('.gyms-list').html(gymsHtml)
-  $('.message').text('Retrieved all wrestling gyms from database')
   $('form').trigger('reset')
   $('#getWrestlingGyms').modal('show')
 }
 const showWrestlingGymsFail = function (error) {
-  $('.message').text('Failed to retrieve gyms from database.\nSee error message: ' + error.message)
   $('form').trigger('reset')
 }
 
 const deleteGymSuccess = function () {
   // After successful delete, trigger a get-all-gyms event to refresh list
   api.showGyms()
-    .then(() => $('#deleteGymSuccess').modal('show'))
-    .then(() => $('.message').text('Deleted gym from database and refreshed list!'))
+    .then(() => $('#deleteGymSuccess').modal({ backdrop: 'static', keyboard: false }))
     .catch(showGymsFail)
 }
 const deleteGymFail = function () {
   $('deleteGymFail').modal('show')
-  $('.message').text('Failed to delete gym from database!')
 }
 
 const showUpdateForm = function () {
@@ -190,12 +166,10 @@ const updateGymSuccess = function (response) {
   api.showGyms()
     .then(() => $('#updateGymModal').modal('hide'))
     .then(() => $('#update-gym').hide())
-    .then(() => $('.message').text('Updated gym info and refreshed list!'))
-    .then(() => $('#updateGymSuccess').modal('show'))
+    .then(() => $('#updateGymSuccess').modal({ backdrop: 'static', keyboard: false }))
     .catch(showGymsFail)
 }
 const updateGymFail = function () {
-  $('.message').text('Failed to update gym!')
   $('#updateGymFail').modal('show')
   $('#updateGymModal').modal('hide')
   $('#update-gym').hide()
